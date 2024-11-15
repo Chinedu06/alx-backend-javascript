9b61b897881748dee2d788e595522278bb8b1723
chinedu06 <godswillchinedu644@gmail.com>
2024-11-15 02:46:08 +0000

    first commit

---
+++ 0x00-ES6_basic/10-main.js
@@ -0,0 +1,7 @@
+// 10-main.js
+
+import appendToEachArrayValue from './10-loops.js';
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+// Expected output: ['correctly-appended', 'correctly-fixed', 'correctly-displayed']
+
