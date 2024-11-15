e37f4ad0f9dc1f4d8b6d1deb8cd863da6e33e54e
chinedu06 <godswillchinedu644@gmail.com>
2024-11-15 10:40:00 +0000

    first commit

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,16 @@
-// 0-constants.js
-
-// Use 'const' for taskFirst as its value won't change
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Use 'let' for taskNext as the variable value will change
+export function getLast() {
+  return ' is okay';
+}
+
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-  
-  return combination;
-}
 
-export function getLast() {
-  return ' is okay';
+  return combination;
 }
 
