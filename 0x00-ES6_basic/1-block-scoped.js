f46050e39315169a0295774132aa344b9a22762b
chinedu06 <godswillchinedu644@gmail.com>
2024-11-15 11:13:29 +0000

    first commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,11 @@
-// 1-block-scoped.js
-
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // Block-scope variable
+  const task2 = true; // Block-scope variable
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped, won't overwrite the outer `task`
-    let task2 = false; // Block-scoped, won't overwrite the outer `task2`
+    return [task, task2];
   }
 
-  return [task, task2];
+  return [];
 }
 
