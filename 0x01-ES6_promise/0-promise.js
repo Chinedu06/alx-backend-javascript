ed0bce950edf9788d56344a2cce5f196c35e7a47
chinedu06 <godswillchinedu644@gmail.com>
2024-11-27 10:08:46 +0000

    first commit

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Resolving the promise without any condition
     resolve();
   });
 }
-
