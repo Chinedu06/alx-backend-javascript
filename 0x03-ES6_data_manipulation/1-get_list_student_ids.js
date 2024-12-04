ae8e1c24a62e7293ccf6f40d8271ecd6b9881f4f
chinedu06 <godswillchinedu644@gmail.com>
2024-12-04 10:25:17 +0000

    first commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,5 @@
 /**
  * Function to get the list of student IDs.
- * @param {Array} students - Array of student objects.
- * @returns {Array} Array of student IDs or an empty 
- * array if input is not an array.
  */
 export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
