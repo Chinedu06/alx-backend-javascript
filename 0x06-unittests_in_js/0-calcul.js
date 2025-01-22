c084c213399fda4a5e06f61b5b69fa4e5bdd743b
chinedu06 <godswillchinedu644@gmail.com>
2025-01-22 13:48:02 +0000

    first commit

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,5 +2,13 @@ function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
 
+function roundAwayFromZero(n) {
+  return n < 0 ? Math.ceil(n - 0.5) : Math.floor(n + 0.5);
+}
+
+function calculateNumber(a, b) {
+  return roundAwayFromZero(a) + roundAwayFromZero(b);
+}
+
 module.exports = calculateNumber;
 
